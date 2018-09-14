22f47c63fbd48c9af9218f09517a76dbb8e0d48f
AmrElabsy <amrelabsy55@gmail.com>
2018-09-14 16:59:41 +0200

    my First Poftifolio

---
+++ JavaScript/Script.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+
+    $("#ser1").mouseenter(function(){
+        $("#servicesbox").css("boxShadow", "0px -50px 300px #292 inset");
+    });
+	$("#ser1").mouseleave(function(){
+        $("#servicesbox").css("boxShadow", "10px 15px 15px #070");
+    });
+
+
+	$("#ser2").mouseenter(function(){
+        $("#servicesbox").css("boxShadow", "0px -50px 300px #FF0 inset");
+    });
+	$("#ser2").mouseleave(function(){
+        $("#servicesbox").css("boxShadow", "10px 15px 15px #070");
+    });
+
+
+    $("#ser3").mouseenter(function(){
+        $("#servicesbox").css("boxShadow", "0px -50px 300px #D00 inset");
+    });
+	$("#ser3").mouseleave(function(){
+        $("#servicesbox").css("boxShadow",  "10px 15px 15px #070");
+    });
+
+
+    $("#ser4").mouseenter(function(){
+        $("#servicesbox").css("boxShadow", "0px -50px 300px #AAF inset");
+    });
+	$("#ser4").mouseleave(function(){
+        $("#servicesbox").css("boxShadow",  "10px 15px 15px #070");
+    });
+
+
+});
